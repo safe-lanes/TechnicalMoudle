4ae64a2fa386876f73cb7d5b1548f21af366deac
Safe Lanes Consultants Pte Ltd <support@sl-sail.com>
2026-02-06 09:50:57 +0000

    fix: resolve merge conflicts in server/routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,4 @@
-import type { Express } from "express";
+atimport type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { getPool, getDb } from "./db";
