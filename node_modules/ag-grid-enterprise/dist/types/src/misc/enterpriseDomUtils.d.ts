4eb6968a59a19b979f7f4dfe95fcf228054c44f2
nabeel87 <naveel.ahmad@safe-lanes.com>
2026-07-13 15:15:41 +0000

    nodmodule and package

--- node_modules/ag-grid-enterprise/dist/types/src/misc/enterpriseDomUtils.d.ts
+++
@@ -1,6 +0,0 @@
-import type { BeanCollection } from 'ag-grid-community';
-/**
- * FF and Safari remove text selections when the focus changes. This is inconsistent with Chrome, whose behaviour
- * we prefer in this case. This utility preserves whatever text selection exists before the given action is taken.
- */
-export declare function _preserveRangesWhile(beans: BeanCollection, fn: () => void): void;
